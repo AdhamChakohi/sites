11c82adf3c1f8c480bee2a7b007aa81a7c512d42
adhamchakohi <amc1150@nyu.edu>
2018-09-25 18:49:48 +0400

    added telepresence test

--- telepresence/scripts.js
+++ telepresence/scripts.js
@@ -1,17 +1,82 @@
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+// function hasGetUserMedia() {
+//   return !!(navigator.mediaDevices &&
+//     navigator.mediaDevices.getUserMedia);
+// }
+
+// if (hasGetUserMedia()) {
+//  	const constraints = {
+//   	video: true
+// 	};
+
+// 	const video = document.querySelector('video');
+
+// 	navigator.mediaDevices.getUserMedia(constraints).
+//  	then((stream) => {video.srcObject = stream});
+// } else {
+//  	alert('getUserMedia() is not supported by your browser');
+// }
+
+const signaling = new SignalingChannel();
+const constraints = {audio: true, video: true};
+const configuration = {iceServers: [{urls: 'stuns:stun.example.org'}]};
+const pc = new RTCPeerConnection(configuration);
+
+// send any ice candidates to the other peer
+pc.onicecandidate = ({candidate}) => signaling.send({candidate});
+
+// let the "negotiationneeded" event trigger offer generation
+pc.onnegotiationneeded = async () => {
+  try {
+    await pc.setLocalDescription(await pc.createOffer());
+    // send the offer to the other peer
+    signaling.send({desc: pc.localDescription});
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// once remote track media arrives, show it in remote video element
+pc.ontrack = (event) => {
+  // don't set srcObject again if it is already set.
+  if (remoteView.srcObject) return;
+  remoteView.srcObject = event.streams[0];
+};
+
+// call start() to initiate
+async function start() {
+  try {
+    // get local stream, show it in self-view and add it to be sent
+    const stream =
+      await navigator.mediaDevices.getUserMedia(constraints);
+    stream.getTracks().forEach((track) =>
+      pc.addTrack(track, stream));
+    selfView.srcObject = stream;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-if (hasGetUserMedia()) {
- 	const constraints = {
-  	video: true
-	};
-
-	const video = document.querySelector('video');
-
-	navigator.mediaDevices.getUserMedia(constraints).
- 	then((stream) => {video.srcObject = stream});
-} else {
- 	alert('getUserMedia() is not supported by your browser');
-}
+signaling.onmessage = async ({desc, candidate}) => {
+  try {
+    if (desc) {
+      // if we get an offer, we need to reply with an answer
+      if (desc.type === 'offer') {
+        await pc.setRemoteDescription(desc);
+        const stream =
+          await navigator.mediaDevices.getUserMedia(constraints);
+        stream.getTracks().forEach((track) =>
+          pc.addTrack(track, stream));
+        await pc.setLocalDescription(await pc.createAnswer());
+        signaling.send({desc: pc.localDescription});
+      } else if (desc.type === 'answer') {
+        await pc.setRemoteDescription(desc);
+      } else {
+        console.log('Unsupported SDP type.');
+      }
+    } else if (candidate) {
+      await pc.addIceCandidate(candidate);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
