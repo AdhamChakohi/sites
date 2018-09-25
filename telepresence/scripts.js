415905154fae92dc870f0e038ae1a10e0caf974e
adhamchakohi <amc1150@nyu.edu>
2018-09-25 17:00:46 +0400

    added telepresnce

---
+++ telepresence/scripts.js
@@ -0,0 +1,17 @@
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+ 	const constraints = {
+  	video: true
+	};
+
+	const video = document.querySelector('video');
+
+	navigator.mediaDevices.getUserMedia(constraints).
+ 	then((stream) => {video.srcObject = stream});
+} else {
+ 	alert('getUserMedia() is not supported by your browser');
+}
