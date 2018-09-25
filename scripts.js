70ce559d6821bcca44f9a1c1561538007d6776e1
Adham <amc1150@nyu.edu>
2018-09-25 14:24:35 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,20 +1,24 @@
 $("#buton").click(function(){
-	$.ajax({
-			url:'https://yesno.wtf/api/',
-			type: 'GET',
-			dataType: 'json',
-			error: function(err){
-				console.log("lmao u failure");
-				console.log(err);
-			},
-			success: function(data){
-				console.log("lmao u did something right");
-				console.log(data.answer);
-				$("#resultAnswer").html(data.answer);
-				var img = "<img src="+data.image+" alt = gif for the answer>"
-				$("#resultImage").html(img);
-				
-			}
+	var userInp = $("#ent").val();
+	if (userInp.length > 0){
+		$.ajax({
+				url:'https://yesno.wtf/api/',
+				type: 'GET',
+				dataType: 'json',
+				error: function(err){
+					console.log("lmao u failure");
+					console.log(err);
+				},
+				success: function(data){
+					console.log("lmao u did something right");
+					console.log(data.answer);
+					$("#resultAnswer").html(data.answer);
+					var img = "<img src="+data.image+" alt = gif for the answer>"
+					$("#resultImage").html(img);
+					
+				}
 
-		});
+			});
+	
+	}
 })
