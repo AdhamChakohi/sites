c5ea721a6c3bb6edcb9c27b888c29a40ffe84085
Adham <amc1150@nyu.edu>
2018-09-25 13:02:51 +0400

    upload website

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$("#buton").click(function(){
+	$.ajax({
+			url:'https://yesno.wtf/api/',
+			type: 'GET',
+			dataType: 'json',
+			error: function(err){
+				console.log("lmao u failure");
+				console.log(err);
+			},
+			success: function(data){
+				console.log("lmao u did something right");
+				console.log(data.answer);
+				$("#resultAnswer").html(data.answer);
+				var img = "<img src="+data.image+" alt = gif for the answer>"
+				$("#resultImage").html(img);
+				
+			}
+
+		});
+})
